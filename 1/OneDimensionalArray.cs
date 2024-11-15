d481b4c84159893909dcd9a11c28370a51a9a1a1
Vitalina <148582544+vitalina-kostenko@users.noreply.github.com>
2024-11-15 21:07:04 +0300

    Add files via upload

---
+++ 1/OneDimensionalArray.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+
+public class OneDimensionalArray
+{
+    private int[] array;
+
+    public OneDimensionalArray(int size)
+    {
+        if (size <= 0)
+        {
+            throw new Exception("Size must be greater than zero.");
+        }
+        array = new int[size];
+    }
+
+    public int this[int index] //одномірний індекатор
+    {
+        get => (index >= 0 && index < array.Length) ? array[index] : throw new Exception("Index is out of range.");
+        set => array[index >= 0 && index < array.Length ? index : throw new Exception("Index is out of range.")] = value;
+    }
+
+    public void InputElements()
+    {
+        Console.WriteLine("Enter elements of the array:");
+        for (int i = 0; i < array.Length; i++)
+            array[i] = int.Parse(Console.ReadLine());
+    }
+
+    public void OutputElements()
+    {
+        Console.WriteLine("Array elements: " + string.Join(" ", array));
+    }
+
+    //public int FindMax()
+    //{
+    //    if (array.Length == 0) throw new Exception("Array is empty.");
+    //    int max = array[0];
+    //    for (int i = 1; i < array.Length; i++)
+    //    {
+    //        if (array[i] > max)
+    //        {
+    //            max = array[i];
+    //        }
+    //    }
+    //    return max;
+    //}
+    public int FindMax() => array.Length == 0 ? throw new Exception("Array is empty.") : array.Max();
+
+    //public int FindMin()
+    //{
+    //    if (array.Length == 0) throw new Exception("Array is empty.");
+    //    int min = array[0];
+    //    for (int i = 1; i < array.Length; i++)
+    //    {
+    //        if (array[i] < min)
+    //        {
+    //            min = array[i];
+    //        }
+    //    }
+    //    return min;
+    //}
+    public int FindMin() => array.Length == 0 ? throw new Exception("Array is empty.") : array.Min();
+}
+
+
+class ArrayOperationsExample
+{
+    public static void Task1()
+    {
+        Console.Write("Enter the size of the array: ");
+        int size = int.Parse(Console.ReadLine());
+
+        OneDimensionalArray array = new OneDimensionalArray(size);
+
+        array.InputElements();
+        array.OutputElements();
+
+        Console.WriteLine("Maximum element: " + array.FindMax());
+        Console.WriteLine("Minimum element: " + array.FindMin());
+    }
+}
+
---
+++ 2/TVector3D.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TVector3D : TVector2D
+{
+    public double Z { get; set; }
+
+    public TVector3D() : base()
+    {
+        Z = 0;
+    }
+
+    public TVector3D(double x, double y, double z) : base(x, y)
+    {
+        Z = z;
+    }
+
+    public TVector3D(TVector3D other) : base(other)
+    {
+        Z = other.Z;
+    }
+
+    public override string ToString() //перевизначення
+    {
+        return $"({X}, {Y}, {Z})";
+    }
+
+    public new double Length()
+    {
+        return Math.Sqrt(X * X + Y * Y + Z * Z);
+    }
+
+    public new void Normalize()
+    {
+        double length = Length();
+        if (length != 0)
+        {
+            X /= length;
+            Y /= length;
+            Z /= length;
+        }
+    }
+
+    public static TVector3D operator +(TVector3D a, TVector3D b)
+    {
+        return new TVector3D(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    }
+
+    public static TVector3D operator -(TVector3D a, TVector3D b)
+    {
+        return new TVector3D(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+    }
+
+    public static double operator *(TVector3D a, TVector3D b)
+    {
+        return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+    }
+
+    public new void Input()
+    {
+        base.Input();
+        Console.Write("Enter Z: ");
+        Z = Convert.ToDouble(Console.ReadLine());
+    }
+
+    public new void Output()
+    {
+        Console.WriteLine($"Vector: {this}");
+    }
+}
